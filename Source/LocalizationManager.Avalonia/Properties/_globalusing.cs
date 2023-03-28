6a9dd2ad38b06b02f71f62488ecb94ddde12f65d
MicroSugar <wufeng4728704@hotmail.com>
2023-03-28 08:58:03 +0800

    add avaloniaui namespace for localized

--- Source/LocalizationManager.Avalonia/Properties/_globalusing.cs
+++ Source/LocalizationManager.Avalonia/Properties/_globalusing.cs
@@ -2,8 +2,7 @@
 global using Avalonia.Data;
 global using Avalonia.Markup.Xaml;
 global using Avalonia.Metadata;
-global using LocalizationManager.Core;
 global using LocalizationManager.Avalonia.Extensions;
-global using System.Reactive.Subjects;
 global using System.Globalization;
+global using System.Reactive.Subjects;
 
