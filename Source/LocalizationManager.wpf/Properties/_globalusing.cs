8b0462a79fcd890fcaf39554e06665b5688bb8ab
MicroSugar <wufeng4728704@hotmail.com>
2023-06-06 18:12:52 +0800

    remove

--- Source/LocalizationManager.wpf/Properties/_globalusing.cs
+++
@@ -1,6 +0,0 @@
-﻿global using System.Windows;
-global using System.Windows.Markup;
-global using System.ComponentModel;
-global using LocalizationManager.Wpf.Converters;
-global using System.Windows.Data;
-global using System.Globalization;
