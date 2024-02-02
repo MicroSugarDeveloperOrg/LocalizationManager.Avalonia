8aa8c741b0ce2b86c71f06084b960b116a54e471
MicroSugar <wufeng4728704@hotmail.com>
2024-02-02 22:23:58 +0800

    add all sln update 2 avalonia 11.0.7

--- Source/Sample/LocalizationManager.Sample.Android/MainActivity.cs
+++ Source/Sample/LocalizationManager.Sample.Android/MainActivity.cs
@@ -1,9 +1,20 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Avalonia;
 using Avalonia.Android;
+using Avalonia.ReactiveUI;
 
 namespace LocalizationManager.Sample.Android;
-[Activity(Label = "LocalizationManager.Sample.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
-public class MainActivity : AvaloniaMainActivity
+[Activity(Label = "LocalizationManager.Sample.Android", 
+          Theme = "@style/MyTheme.NoActionBar", 
+          Icon = "@drawable/icon", 
+          LaunchMode = LaunchMode.SingleTop, 
+          ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+public class MainActivity : AvaloniaMainActivity<App>
 {
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .UseReactiveUI();
+    }
 }
